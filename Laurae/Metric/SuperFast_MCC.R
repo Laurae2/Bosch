f0a78311f3f40960faebc16d90fc918dd0ac781e
Laurae2 <Laurae2@users.noreply.github.com>
2016-09-23 18:12:51 +0200

    Added DT version

--- Laurae/Metric/SuperFast_MCC.R
+++ Laurae/Metric/SuperFast_MCC.R
@@ -7,7 +7,33 @@ mcc <- function(TP, FP, FN, TN)
     return(ifelse(den == 0, 0, num/sqrt(den)))
 }
 
-eval_mmc <- function(y_true, y_prob, show=F) {
+library(data.table)
+eval_mmcDT <- function(y_true, y_prob, show=F) {
+  DT <- data.table(y_true = y_true, y_prob = y_prob, key="y_prob")
+
+  nump <- sum(y_true)
+  numn <- length(y_true)- nump
+
+  DT[, tn_v:= cumsum(y_true == 0)]
+  DT[, fp_v:= cumsum(y_true == 1)]
+  DT[, fn_v:= numn - tn_v]
+  DT[, tp_v:= nump - fp_v]
+  DT[, tp_v:= nump - fp_v]
+  DT[, mcc_v:= (tp_v * tn_v - fp_v * fn_v) / sqrt((tp_v + fp_v) * (tp_v + fn_v) * (tn_v + fp_v) * (tn_v + fn_v))]
+  DT[, mcc_v:= ifelse(!is.finite(mcc_v), 0, mcc_v)]
+
+  if (!show) return(max(DT[['mcc_v']]))
+
+  best_mcc <- max(DT[['mcc_v']])
+  best_proba <- DT[['y_prob']][which.max(DT[['mcc_v']])]
+  y_pred <- as.numeric(y_prob > best_proba)
+  score <- mcc(y_true, y_pred)
+  cat("\n",score, best_mcc)
+  return(list(best_proba, best_mcc, y_pred))
+}
+
+
+eval_mmcDF <- function(y_true, y_prob, show=F) {
   idx <- order(y_prob)
   y_prob_sort <- y_prob[idx]
   y_true_sort <- y_true[idx]
