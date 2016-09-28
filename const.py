9994c3bf29dff04905ca6a9430727c26b2f6de94
Joost Bloom <bloomspam@gmail.com>
2016-09-27 20:09:52 -0700

    fix: updated utility files

--- const.py
+++ const.py
@@ -8,6 +8,10 @@ Created on Wed Sep 21 11:31:13 2016
 
 BASE_PATH = '/Volumes/My Book/kaggle_bosch/'
 
+DATA_PATH = './data/'
+
+AUTHOR = 'joostgp'
+
 TRAIN_FILES = ['train_numeric',
                'train_categorical_to_num',
                'train_date']
@@ -15,3 +19,5 @@ TRAIN_FILES = ['train_numeric',
 TEST_FILES = ['test_numeric',
               'test_categorical_to_num',
               'test_date']
+              
+LOOK_UP_TABLE = DATA_PATH + 'date_feat_lut_V2.csv'
