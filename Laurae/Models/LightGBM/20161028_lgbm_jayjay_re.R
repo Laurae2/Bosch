220799e4eda074725bc7ceaad758606fbd229613
Laurae <Laurae2@users.noreply.github.com>
2016-10-28 19:03:17 +0200

    Create 20161028_lgbm_jayjay_re.R

---
+++ Laurae/Models/LightGBM/20161028_lgbm_jayjay_re.R
@@ -0,0 +1,402 @@
+# Libraries
+
+library(data.table)
+library(Matrix)
+library(recommenderlab)
+library(Laurae)
+library(fastdigest)
+library(pbapply)
+library(ggplot2)
+library(R.utils)
+library(stringi)
+
+setwd("E:/")
+
+my_script_is_using <- "E:/Laurae/20161028_lgbm_jayjay_re"
+my_lgbm_is_at <- "C:/Compiled/LightGBM/windows/x64/Release/lightgbm.exe"
+my_script_subbed <- basename(my_script_is_using)
+threads <- 12
+leaves <- 255
+eta <- 0.05
+min_sample <- 100
+min_hess <- 10
+subsample <- 1.0
+colsample <- 1.0
+sampling_freq <- ifelse(subsample == 1, 0, 1)
+
+
+# Load from RDS
+
+train <- readRDS("Laurae/jayjay_clean/train.rds")
+test <- readRDS("Laurae/jayjay_clean/test.rds")
+label <- readRDS("datasets/labels.rds")
+
+train[, Id := NULL]
+test[, Id := NULL]
+gc()
+
+StratifiedCV <- function(Y, folds, seed) {
+  folded <- list()
+  folded1 <- list()
+  folded2 <- list()
+  set.seed(seed)
+  temp_Y0 <- which(Y == 0)
+  temp_Y1 <- which(Y == 1)
+  for (i in 1:folds) {
+    folded1[[i]] <- sample(temp_Y0, floor(length(temp_Y0) / ((folds + 1) - i)))
+    temp_Y0 <- temp_Y0[!temp_Y0 %in% folded1[[i]]]
+    folded2[[i]] <- sample(temp_Y1, floor(length(temp_Y1) / ((folds + 1) - i)))
+    temp_Y1 <- temp_Y1[!temp_Y1 %in% folded2[[i]]]
+    folded[[i]] <- c(folded1[[i]], folded2[[i]])
+  }
+  return(folded)
+}
+
+folds <- StratifiedCV(label, 5, 11111)
+
+
+# Real cannon is here
+
+temp_model <- lgbm.cv(y_train = label,
+                      x_train = train,
+                      x_test = test,
+                      data_has_label = TRUE,
+                      NA_value = "nan",
+                      lgbm_path = my_lgbm_is_at,
+                      workingdir = my_script_is_using,
+                      files_exist = FALSE,
+                      save_binary = FALSE,
+                      validation = TRUE,
+                      folds = folds,
+                      predictions = TRUE,
+                      importance = TRUE,
+                      full_quiet = FALSE,
+                      verbose = FALSE,
+                      num_threads = threads,
+                      application = "binary",
+                      learning_rate = eta,
+                      num_iterations = 500,
+                      early_stopping_rounds = 75,
+                      num_leaves = leaves,
+                      min_data_in_leaf = min_sample,
+                      min_sum_hessian_in_leaf = min_hess,
+                      max_bin = 255,
+                      feature_fraction = colsample,
+                      bagging_fraction = subsample,
+                      bagging_freq = sampling_freq,
+                      is_unbalance = FALSE,
+                      metric = "auc",
+                      is_training_metric = TRUE)
+
+saveRDS(temp_model, file.path(my_script_is_using, "aaa_LightGBM_cv.rds"), compress = TRUE)
+
+gc()
+
+#temp_model <- readRDS(file.path(my_script_is_using, "aaa_LightGBM_cv.rds"))
+
+best_iter <- 0
+for (j in 1:5) {
+  best_iter <- best_iter + 0.2 * temp_model$Models[[j]]$Best
+}
+
+best_model <- lgbm.train(y_train = label,
+                         x_train = train,
+                         x_test = test,
+                         data_has_label = TRUE,
+                         NA_value = "nan",
+                         lgbm_path = my_lgbm_is_at,
+                         workingdir = my_script_is_using,
+                         files_exist = FALSE,
+                         save_binary = FALSE,
+                         predictions = TRUE,
+                         importance = TRUE,
+                         full_quiet = FALSE,
+                         verbose = TRUE,
+                         num_threads = threads,
+                         application = "binary",
+                         learning_rate = eta,
+                         num_iterations = floor(best_iter * 1.1),
+                         num_leaves = leaves,
+                         min_data_in_leaf = min_sample,
+                         min_sum_hessian_in_leaf = min_hess,
+                         max_bin = 255,
+                         feature_fraction = colsample,
+                         bagging_fraction = subsample,
+                         bagging_freq = sampling_freq,
+                         is_unbalance = FALSE,
+                         metric = "auc",
+                         is_training_metric = TRUE)
+
+saveRDS(best_model, file.path(my_script_is_using, "aaa_LightGBM_full.rds"), compress = TRUE)
+
+
+
+mcc_fixed <- function(y_prob, y_true, prob) {
+  
+  positives <- as.logical(y_true) # label to boolean
+  counter <- sum(positives) # get the amount of positive labels
+  tp <- as.numeric(sum(y_prob[positives] > prob))
+  fp <- as.numeric(sum(y_prob[!positives] > prob))
+  tn <- as.numeric(length(y_true) - counter - fp) # avoid computing he opposite
+  fn <- as.numeric(counter - tp) # avoid computing the opposite
+  mcc <- (tp * tn - fp * fn) / (sqrt((tp + fp) * (tp + fn) * (tn + fp) * (tn + fn)))
+  mcc <- ifelse(is.na(mcc), -1, mcc)
+  return(mcc)
+  
+}
+
+mcc_eval_print <- function(y_prob, y_true) {
+  y_true <- y_true
+  
+  DT <- data.table(y_true = y_true, y_prob = y_prob, key = "y_prob")
+  
+  nump <- sum(y_true)
+  numn <- length(y_true) - nump
+  
+  DT[, tn_v := as.numeric(cumsum(y_true == 0))]
+  DT[, fp_v := cumsum(y_true == 1)]
+  DT[, fn_v := numn - tn_v]
+  DT[, tp_v := nump - fp_v]
+  DT[, tp_v := nump - fp_v]
+  DT[, mcc_v := (tp_v * tn_v - fp_v * fn_v) / sqrt((tp_v + fp_v) * (tp_v + fn_v) * (tn_v + fp_v) * (tn_v + fn_v))]
+  DT[, mcc_v := ifelse(!is.finite(mcc_v), 0, mcc_v)]
+  gc(verbose = FALSE)
+  
+  return(max(DT[['mcc_v']]))
+  
+}
+
+mcc_eval_pred <- function(y_prob, y_true) {
+  y_true <- y_true
+  
+  DT <- data.table(y_true = y_true, y_prob = y_prob, key = "y_prob")
+  
+  nump <- sum(y_true)
+  numn <- length(y_true) - nump
+  
+  DT[, tn_v := as.numeric(cumsum(y_true == 0))]
+  DT[, fp_v := cumsum(y_true == 1)]
+  DT[, fn_v := numn - tn_v]
+  DT[, tp_v := nump - fp_v]
+  DT[, tp_v := nump - fp_v]
+  DT[, mcc_v := (tp_v * tn_v - fp_v * fn_v) / sqrt((tp_v + fp_v) * (tp_v + fn_v) * (tn_v + fp_v) * (tn_v + fn_v))]
+  DT[, mcc_v := ifelse(!is.finite(mcc_v), 0, mcc_v)]
+  
+  return(DT[['y_prob']][which.max(DT[['mcc_v']])])
+  
+}
+
+FastROC <- function(y, x) {
+  
+  # y = actual
+  # x = predicted
+  x1 = as.numeric(x[y == 1])
+  n1 = as.numeric(length(x1))
+  x2 = as.numeric(x[y == 0])
+  n2 = as.numeric(length(x2))
+  r = rank(c(x1,x2))
+  return((sum(r[1:n1]) - n1 * (n1 + 1) / 2) / (n1 * n2))
+  
+}
+
+
+
+
+# Know what is inside
+
+
+AnalysisFunc <- function(lgbm, label, folds, validationValues, predictedValuesCV, predictedValues) {
+  # lgbm = your LightGBM cross-validated model (set as "NA" if it is not a LGBM model)
+  # Label = your label
+  # Folds = your fold list
+  # validationValues = your validation values (out of fold predictions)
+  # predictedValuesCV = your predicted values (on test data) from CV (set as "NA" if you don't have any)
+  # predictedValues = your prediction values (on test data) on a model trained on all data
+  
+  if (length(lgbm) > 1) {
+    
+    # Print feature importance to file
+    
+    jpeg(filename = file.path(my_script_is_using, "importance_log_small.jpg"), width = 760, height = 894, units = "px", pointsize = 12)
+    lgbm.fi.plot(temp_model, n_best = 50, no_log = FALSE, is.cv = TRUE, multipresence = TRUE, plot = TRUE)
+    dev.off()
+    jpeg(filename = file.path(my_script_is_using, "importance_nonlog_small.jpg"), width = 760, height = 894, units = "px", pointsize = 12)
+    lgbm.fi.plot(temp_model, n_best = 50, no_log = TRUE, is.cv = TRUE, multipresence = TRUE, plot = TRUE)
+    dev.off()
+    
+    jpeg(filename = file.path(my_script_is_using, "importance_log_big.jpg"), width = 760, height = 1788, units = "px", pointsize = 12)
+    lgbm.fi.plot(temp_model, n_best = 100, no_log = FALSE, is.cv = TRUE, multipresence = TRUE, plot = TRUE)
+    dev.off()
+    jpeg(filename = file.path(my_script_is_using, "importance_nonlog_big.jpg"), width = 760, height = 1788, units = "px", pointsize = 12)
+    lgbm.fi.plot(temp_model, n_best = 100, no_log = TRUE, is.cv = TRUE, multipresence = TRUE, plot = TRUE)
+    dev.off()
+    
+  }
+  
+  # Setup tee
+  sink(file = file.path(my_script_is_using, "diagnostics.txt"), append = FALSE, split = TRUE)
+  
+  if (length(lgbm) > 1) {
+    
+    # Do best feature listing
+    cat("Cross-validated best features list (all used features to copy & paste):\n")
+    mini_model <- copy(lgbm$FeatureImp)
+    mini_model <- mini_model[Freq == 5, ]
+    dput(mini_model$Feature)
+    cat("\nSee the screenshots for more accuracy about the gain\n\n\n")
+    
+    # Get iteration information
+    temp_iter <- numeric(5)
+    best_iter <- 0
+    for (j in 1:5) {
+      temp_iter[j] <- lgbm$Models[[j]]$Best
+      best_iter <- best_iter + 0.2 * temp_iter[j]
+      cat("Fold ", j, " converged after ", temp_iter[j], " iterations.\n", sep = "")
+    }
+    cat("Iterations: ", mean(temp_iter), " + ", sd(temp_iter), "\n\n\n", sep = "")
+    
+  }
+  
+  # Get AUC metric information
+  temp_auc <- numeric(length(folds))
+  best_auc <- 0
+  for (j in 1:length(folds)) {
+    temp_auc[j] <- FastROC(y = label[folds[[j]]], x = validationValues[folds[[j]]])
+    best_auc <- best_auc + (temp_auc[j] / length(folds))
+    cat("Fold ", j, ": AUC=", temp_auc[j], "\n", sep = "")
+  }
+  cat("AUC: ", mean(temp_auc), " + ", sd(temp_auc), "\n", sep = "")
+  cat("Average AUC using all data: ", FastROC(y = label, x = validationValues), "\n\n\n", sep = "")
+  
+  
+  # Get MCC metric information
+  temp_mcc <- numeric(length(folds))
+  temp_thresh <- numeric(length(folds))
+  temp_positives <- numeric(length(folds))
+  temp_detection <- numeric(length(folds))
+  best_mcc <- 0
+  for (j in 1:length(folds)) {
+    
+    temp_mcc[j] <- mcc_eval_print(y_prob = validationValues[folds[[j]]], y_true = label[folds[[j]]])
+    temp_thresh[j] <- mcc_eval_pred(y_prob = validationValues[folds[[j]]], y_true = label[folds[[j]]])
+    temp_positives[j] <- sum(validationValues[folds[[j]]] > temp_thresh[[j]])
+    temp_detection[j] <- temp_positives[j] / sum(label[folds[[j]]])
+    best_mcc <- best_mcc + (temp_mcc[j] / length(folds))
+    cat("Fold ", j, ": MCC=", temp_mcc[j], " (", temp_positives[j], " [", sprintf("%05.2f", temp_detection[j] * 100), "%] positives), threshold=", temp_thresh[j], "\n", sep = "")
+    
+  }
+  cat("MCC: ", mean(temp_mcc), " + ", sd(temp_mcc), "\n", sep = "")
+  cat("Positives: ", mean(temp_positives), " + ", sd(temp_positives), "\n", sep = "")
+  cat("Detection Rate: ", mean(temp_detection), " + ", sd(temp_detection), "\n", sep = "")
+  cat("Average MCC on all data (5 fold): ", mcc_fixed(y_prob = validationValues, y_true = label, prob = mean(temp_mcc)), ", threshold=", mean(temp_mcc), "\n", sep = "")
+  cat("Average MCC using all data: ", mcc_eval_print(y_prob = validationValues, y_true = label), ", threshold=", mcc_eval_pred(y_prob = validationValues, y_true = label), "\n\n\n", sep = "")
+  
+  
+  if (length(predictedValuesCV) > 1) {
+    
+    # Create overfitted submission from all data
+    best_mcc1 <- mcc_eval_pred(y_prob = validationValues, y_true = label)
+    submission0_ov <- fread("datasets/sample_submission.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+    submission0_ov$Response <- as.numeric(predictedValuesCV > best_mcc1)
+    best_count1 <- sum(submission0_ov$Response == 1)
+    cat("Submission overfitted threshold on all MCC positives: ", best_count1, "\n\n", sep = "")
+    write.csv(submission0_ov, file = file.path(my_script_is_using, paste(my_script_subbed, "_val_", sprintf("%.06f", best_mcc1), "_", best_count1, ".csv", sep = "")), row.names = FALSE)
+    
+    # Create CV submission from validation
+    best_mcc2 <- best_mcc
+    submission0 <- fread("datasets/sample_submission.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+    submission0$Response <- as.numeric(predictedValuesCV > best_mcc2)
+    best_count2 <- sum(submission0$Response == 1)
+    cat("Submission average validated threshold on all MCC positives: ", best_count2, "\n\n", sep = "")
+    write.csv(submission0, file = file.path(my_script_is_using, paste(my_script_subbed, "_val_", sprintf("%.06f", best_mcc2), "_", best_count2, ".csv", sep = "")), row.names = FALSE)
+    
+    # Create average of the two previous submissions
+    best_mcc3 <- (best_mcc1 + best_mcc2) / 2
+    submission0_ex <- fread("datasets/sample_submission.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+    submission0_ex$Response <- as.numeric(predictedValuesCV > best_mcc3)
+    best_count3 <- sum(submission0_ex$Response == 1)
+    cat("Submission average of overfit+validated threshold positives: ", best_count3, "\n\n", sep = "")
+    write.csv(submission0_ex, file = file.path(my_script_is_using, paste(my_script_subbed, "_val_", sprintf("%.06f", best_mcc3), "_", best_count3, ".csv", sep = "")), row.names = FALSE)
+    
+    # Create files for stacker
+    write.csv(validationValues, file = file.path(my_script_is_using, "aaa_stacker_preds_train_headerY.csv"), row.names = FALSE)
+    write.csv(predictedValuesCV, file = file.path(my_script_is_using, "aaa_stacker_preds_test_headerY.csv"), row.names = FALSE)
+    
+  }
+  
+  
+  
+  # Create overfitted submission from all data using full trained model
+  best_mcc1_all <- best_mcc1
+  submission0_ov_all <- fread("datasets/sample_submission.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+  submission0_ov_all$Response <- as.numeric(predictedValues > best_mcc1_all)
+  best_count1_all <- sum(submission0_ov_all$Response == 1)
+  cat("Submission with all data overfitted threshold on all MCC positives: ", best_count1_all, ". Threshold=", best_mcc1_all, "\n\n", sep = "")
+  write.csv(submission0_ov_all, file = file.path(my_script_is_using, paste(my_script_subbed, "_all_", sprintf("%.06f", best_mcc1_all), "_", best_count1_all, ".csv", sep = "")), row.names = FALSE)
+  
+  # Create CV submission from validation using full trained model
+  best_mcc2_all <- best_mcc2
+  submission0_all <- fread("datasets/sample_submission.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+  submission0_all$Response <- as.numeric(predictedValues > best_mcc2_all)
+  best_count2_all <- sum(submission0_all$Response == 1)
+  cat("Submission with all data average validated threshold on all MCC positives: ", best_count2_all, ". Threshold=", best_mcc2_all, "\n\n", sep = "")
+  write.csv(submission0_all, file = file.path(my_script_is_using, paste(my_script_subbed, "_all_", sprintf("%.06f", best_mcc2_all), "_", best_count2_all, ".csv", sep = "")), row.names = FALSE)
+  
+  # Create average of the two previous submissions using full trained model
+  best_mcc3_all <- best_mcc3
+  submission0_ex_all <- fread("datasets/sample_submission.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+  submission0_ex_all$Response <- as.numeric(predictedValues > best_mcc3_all)
+  best_count3_all <- sum(submission0_ex_all$Response == 1)
+  cat("Submission with all data average of overfit+validated threshold positives: ", best_count3_all, ". Threshold=", best_mcc3_all, "\n\n", sep = "")
+  write.csv(submission0_ex_all, file = file.path(my_script_is_using, paste(my_script_subbed, "_all_", sprintf("%.06f", best_mcc3_all), "_", best_count3_all, ".csv", sep = "")), row.names = FALSE)
+  
+  
+  
+  mini_preds <- predictedValues
+  mini_preds <- mini_preds[order(mini_preds, decreasing = TRUE)]
+  
+  # Create overfitted submission from all data using full trained model using respective positive count
+  best_mcc1_all_val <- mini_preds[best_count1 + 1]
+  submission0_ov_all_val <- fread("datasets/sample_submission.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+  submission0_ov_all_val$Response <- as.numeric(predictedValues > best_mcc1_all_val)
+  best_count1_all_val <- sum(submission0_ov_all_val$Response == 1)
+  cat("Submission with all data by taking the amount of positives of overfitted threshold on all MCC positives: ", best_count1_all_val, ". Threshold=", best_mcc1_all_val, "\n\n", sep = "")
+  write.csv(submission0_ov_all_val, file = file.path(my_script_is_using, paste(my_script_subbed, "_all_val_", sprintf("%.06f", best_mcc1_all_val), "_", best_count1_all_val, ".csv", sep = "")), row.names = FALSE)
+  
+  # Create CV submission from validation using full trained model using respective positive count
+  best_mcc2_all_val <- mini_preds[best_count2 + 1]
+  submission0_all_val <- fread("datasets/sample_submission.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+  submission0_all_val$Response <- as.numeric(predictedValues > best_mcc2_all_val)
+  best_count2_all_val <- sum(submission0_all_val$Response == 1)
+  cat("Submission with all data by taking the amount of positives of average validated threshold on all MCC positives: ", best_count2_all_val, ". Threshold=", best_mcc2_all_val, "\n\n", sep = "")
+  write.csv(submission0_all_val, file = file.path(my_script_is_using, paste(my_script_subbed, "_all_val_", sprintf("%.06f", best_mcc2_all_val), "_", best_count2_all_val, ".csv", sep = "")), row.names = FALSE)
+  
+  # Create average of the two previous submissions using full trained model using respective positive count
+  best_mcc3_all_val <- mini_preds[best_count3 + 1]
+  submission0_ex_all_val <- fread("datasets/sample_submission.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+  submission0_ex_all_val$Response <- as.numeric(predictedValues > best_mcc3_all_val)
+  best_count3_all_val <- sum(submission0_ex_all_val$Response == 1)
+  cat("Submission with all data by taking the amount of positives of average of overfit+validated threshold positives: ", best_count3_all_val, ". Threshold=", best_mcc3_all_val, "\n\n", sep = "")
+  write.csv(submission0_ex_all_val, file = file.path(my_script_is_using, paste(my_script_subbed, "_all_val_", sprintf("%.06f", best_mcc3_all_val), "_", best_count3_all_val, ".csv", sep = "")), row.names = FALSE)
+  
+  # Create submissions using full trained model using total validated positive count
+  best_mcc_extra <- mini_preds[sum(temp_positives) + 1]
+  submission0_extra <- fread("datasets/sample_submission.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+  submission0_extra$Response <- as.numeric(predictedValues > best_mcc_extra)
+  best_count_extra <- sum(submission0_extra$Response == 1)
+  cat("Submission with all data by taking the sum of positives of validated threshold positives: ", best_count_extra, ". Threshold=", best_mcc_extra, "\n\n", sep = "")
+  write.csv(submission0_extra, file = file.path(my_script_is_using, paste(my_script_subbed, "_extra_", sprintf("%.06f", best_mcc_extra), "_", best_count_extra, ".csv", sep = "")), row.names = FALSE)
+  
+  sink()
+  
+  return("Done")
+  
+}
+
+
+AnalysisFunc(lgbm = temp_model,
+             label = label,
+             folds = folds,
+             validationValues = temp_model$Validation[[1]],
+             predictedValuesCV = temp_model$Testing[[1]],
+             predictedValues = best_model$Testing)
