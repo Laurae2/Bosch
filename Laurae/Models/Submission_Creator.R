7551df9e63fdf02e12ff7dd78fb96dfea3efe46d
Laurae <Laurae2@users.noreply.github.com>
2016-11-03 12:23:52 +0100

    Auto-submission creator added

---
+++ Laurae/Models/Submission_Creator.R
@@ -0,0 +1,160 @@
+
+AnalysisFunc <- function(label, folds, validationValues, predictedValuesCV, predictedValues) {
+  # label = your label, mandatory
+  # folds = your fold list, mandatory
+  # validationValues = your validation (out of fold) values (out of fold predictions), mandatory
+  # predictedValuesCV = your averaged predicted (test) values (on test data) from CV (set as "NA" if you don't have any)
+  # predictedValues = your prediction (test) values (on test data) on a model trained on all data (set as "NA" if you don't have any)
+  
+  # Setup Markdown
+  cat("```r\n")
+  
+  # Get AUC metric information
+  temp_auc <- numeric(length(folds))
+  best_auc <- 0
+  for (j in 1:length(folds)) {
+    temp_auc[j] <- FastROC(y = label[folds[[j]]], x = validationValues[folds[[j]]])
+    best_auc <- best_auc + (temp_auc[j] / length(folds))
+    cat("Fold ", j, ": AUC=", sprintf("%.07f", temp_auc[j]), "\n", sep = "")
+  }
+  cat("AUC: ", sprintf("%.07f", mean(temp_auc)), " + ", sprintf("%.07f", sd(temp_auc)), "\n", sep = "")
+  cat("Average AUC using all data: ", sprintf("%.07f", FastROC(y = label, x = validationValues)), "\n\n\n", sep = "")
+  
+  
+  # Get MCC metric information
+  temp_mcc <- numeric(length(folds))
+  temp_thresh <- numeric(length(folds))
+  temp_positives <- numeric(length(folds))
+  temp_detection <- numeric(length(folds))
+  temp_true <- numeric(length(folds))
+  temp_undetect <- numeric(length(folds))
+  best_mcc <- 0
+  for (j in 1:length(folds)) {
+    
+    temp_mcc[j] <- mcc_eval_print(y_prob = validationValues[folds[[j]]], y_true = label[folds[[j]]])
+    temp_thresh[j] <- mcc_eval_pred(y_prob = validationValues[folds[[j]]], y_true = label[folds[[j]]])
+    mini_preds <- validationValues[folds[[j]]] > temp_thresh[[j]]
+    temp_positives[j] <- sum(mini_preds)
+    temp_detection[j] <- 100 * temp_positives[j] / sum(label[folds[[j]]])
+    temp_true[j] <- sum((mini_preds[mini_preds == TRUE] == label[folds[[j]]][mini_preds == TRUE]))
+    temp_undetect[j] <- sum(label[folds[[j]]]) - temp_true[j]
+    temp_true[j] <- 100 * temp_true[j] / sum(length(mini_preds[mini_preds == TRUE]))
+    best_mcc <- best_mcc + (temp_mcc[j] / length(folds))
+    cat("Fold ", j, ": MCC=", sprintf("%.07f", temp_mcc[j]), " (", sprintf("%04d", temp_positives[j]), " [", sprintf("%05.2f", temp_detection[j]), "%] positives), threshold=", sprintf("%.07f", temp_thresh[j]), " => True positives = ", sprintf("%06.3f", temp_true[j]), "%\n", sep = "")
+    
+  }
+  
+  cat("MCC: ", sprintf("%.07f", mean(temp_mcc)), " + ", sprintf("%.07f", sd(temp_mcc)), "\n", sep = "")
+  cat("Threshold: ", sprintf("%.07f", mean(temp_thresh)), " + ", sprintf("%.07f", sd(temp_thresh)), "\n", sep = "")
+  cat("Positives: ", sprintf("%06.2f", mean(temp_positives)), " + ", sprintf("%06.2f", sd(temp_positives)), "\n", sep = "")
+  cat("Detection Rate %: ", sprintf("%06.3f", mean(temp_detection)), " + ", sprintf("%06.3f", sd(temp_detection)), "\n", sep = "")
+  cat("True positives %: ", sprintf("%06.3f", mean(temp_true)), " + ", sprintf("%06.3f", sd(temp_true)), "\n", sep = "")
+  cat("Undetected positives: ", sprintf("%07.2f", mean(temp_undetect)), " + ", sprintf("%07.2f", sd(temp_undetect)), "\n", sep = "")
+  cat("Average MCC on all data (5 fold): ", sprintf("%.07f", mcc_fixed(y_prob = validationValues, y_true = label, prob = mean(temp_thresh))), ", threshold=", sprintf("%.07f", mean(temp_thresh)), "\n", sep = "")
+  cat("Average MCC using all data: ", sprintf("%.07f", mcc_eval_print(y_prob = validationValues, y_true = label)), ", threshold=", sprintf("%.07f", mcc_eval_pred(y_prob = validationValues, y_true = label)), "\n\n\n", sep = "")
+  
+  
+  if (length(predictedValuesCV) > 1) {
+    
+    # Create overfitted submission from all data
+    best_mcc1 <- mcc_eval_pred(y_prob = validationValues, y_true = label)
+    submission0_ov <- fread("datasets/sample_submission.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+    submission0_ov$Response <- as.numeric(predictedValuesCV > best_mcc1)
+    best_count1 <- sum(submission0_ov$Response == 1)
+    cat("Submission overfitted threshold on all MCC positives: ", best_count1, "\n\n", sep = "")
+    write.csv(submission0_ov, file = file.path(my_script_is_using, paste(my_script_subbed, "_val_", sprintf("%.06f", best_mcc1), "_", best_count1, ".csv", sep = "")), row.names = FALSE)
+    
+    # Create CV submission from validation
+    best_mcc2 <- mean(temp_thresh)
+    submission0 <- fread("datasets/sample_submission.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+    submission0$Response <- as.numeric(predictedValuesCV > best_mcc2)
+    best_count2 <- sum(submission0$Response == 1)
+    cat("Submission average validated threshold on all MCC positives: ", best_count2, "\n\n", sep = "")
+    write.csv(submission0, file = file.path(my_script_is_using, paste(my_script_subbed, "_val_", sprintf("%.06f", best_mcc2), "_", best_count2, ".csv", sep = "")), row.names = FALSE)
+    
+    # Create average of the two previous submissions
+    best_mcc3 <- (best_mcc1 + best_mcc2) / 2
+    submission0_ex <- fread("datasets/sample_submission.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+    submission0_ex$Response <- as.numeric(predictedValuesCV > best_mcc3)
+    best_count3 <- sum(submission0_ex$Response == 1)
+    cat("Submission average of overfit+validated threshold positives: ", best_count3, "\n\n", sep = "")
+    write.csv(submission0_ex, file = file.path(my_script_is_using, paste(my_script_subbed, "_val_", sprintf("%.06f", best_mcc3), "_", best_count3, ".csv", sep = "")), row.names = FALSE)
+    
+    # Create files for stacker
+    write.csv(validationValues, file = file.path(my_script_is_using, "aaa_stacker_preds_train_headerY.csv"), row.names = FALSE)
+    write.csv(predictedValuesCV, file = file.path(my_script_is_using, "aaa_stacker_preds_test_headerY.csv"), row.names = FALSE)
+    
+  }
+  
+  
+  if (length(predictedValues) > 1) {
+    
+    # Create overfitted submission from all data using full trained model
+    best_mcc1_all <- best_mcc1
+    submission0_ov_all <- fread("datasets/sample_submission.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+    submission0_ov_all$Response <- as.numeric(predictedValues > best_mcc1_all)
+    best_count1_all <- sum(submission0_ov_all$Response == 1)
+    cat("Submission with all data overfitted threshold on all MCC positives: ", best_count1_all, ". Threshold=", best_mcc1_all, "\n\n", sep = "")
+    write.csv(submission0_ov_all, file = file.path(my_script_is_using, paste(my_script_subbed, "_all_", sprintf("%.06f", best_mcc1_all), "_", best_count1_all, ".csv", sep = "")), row.names = FALSE)
+    
+    # Create CV submission from validation using full trained model
+    best_mcc2_all <- best_mcc2
+    submission0_all <- fread("datasets/sample_submission.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+    submission0_all$Response <- as.numeric(predictedValues > best_mcc2_all)
+    best_count2_all <- sum(submission0_all$Response == 1)
+    cat("Submission with all data average validated threshold on all MCC positives: ", best_count2_all, ". Threshold=", best_mcc2_all, "\n\n", sep = "")
+    write.csv(submission0_all, file = file.path(my_script_is_using, paste(my_script_subbed, "_all_", sprintf("%.06f", best_mcc2_all), "_", best_count2_all, ".csv", sep = "")), row.names = FALSE)
+    
+    # Create average of the two previous submissions using full trained model
+    best_mcc3_all <- best_mcc3
+    submission0_ex_all <- fread("datasets/sample_submission.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+    submission0_ex_all$Response <- as.numeric(predictedValues > best_mcc3_all)
+    best_count3_all <- sum(submission0_ex_all$Response == 1)
+    cat("Submission with all data average of overfit+validated threshold positives: ", best_count3_all, ". Threshold=", best_mcc3_all, "\n\n", sep = "")
+    write.csv(submission0_ex_all, file = file.path(my_script_is_using, paste(my_script_subbed, "_all_", sprintf("%.06f", best_mcc3_all), "_", best_count3_all, ".csv", sep = "")), row.names = FALSE)
+    
+    
+    
+    mini_preds <- predictedValues
+    mini_preds <- mini_preds[order(mini_preds, decreasing = TRUE)]
+    
+    # Create overfitted submission from all data using full trained model using respective positive count
+    best_mcc1_all_val <- mini_preds[best_count1 + 1]
+    submission0_ov_all_val <- fread("datasets/sample_submission.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+    submission0_ov_all_val$Response <- as.numeric(predictedValues > best_mcc1_all_val)
+    best_count1_all_val <- sum(submission0_ov_all_val$Response == 1)
+    cat("Submission with all data by taking the amount of positives of overfitted threshold on all MCC positives: ", best_count1_all_val, ". Threshold=", best_mcc1_all_val, "\n\n", sep = "")
+    write.csv(submission0_ov_all_val, file = file.path(my_script_is_using, paste(my_script_subbed, "_all_val_", sprintf("%.06f", best_mcc1_all_val), "_", best_count1_all_val, ".csv", sep = "")), row.names = FALSE)
+    
+    # Create CV submission from validation using full trained model using respective positive count
+    best_mcc2_all_val <- mini_preds[best_count2 + 1]
+    submission0_all_val <- fread("datasets/sample_submission.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+    submission0_all_val$Response <- as.numeric(predictedValues > best_mcc2_all_val)
+    best_count2_all_val <- sum(submission0_all_val$Response == 1)
+    cat("Submission with all data by taking the amount of positives of average validated threshold on all MCC positives: ", best_count2_all_val, ". Threshold=", best_mcc2_all_val, "\n\n", sep = "")
+    write.csv(submission0_all_val, file = file.path(my_script_is_using, paste(my_script_subbed, "_all_val_", sprintf("%.06f", best_mcc2_all_val), "_", best_count2_all_val, ".csv", sep = "")), row.names = FALSE)
+    
+    # Create average of the two previous submissions using full trained model using respective positive count
+    best_mcc3_all_val <- mini_preds[best_count3 + 1]
+    submission0_ex_all_val <- fread("datasets/sample_submission.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+    submission0_ex_all_val$Response <- as.numeric(predictedValues > best_mcc3_all_val)
+    best_count3_all_val <- sum(submission0_ex_all_val$Response == 1)
+    cat("Submission with all data by taking the amount of positives of average of overfit+validated threshold positives: ", best_count3_all_val, ". Threshold=", best_mcc3_all_val, "\n\n", sep = "")
+    write.csv(submission0_ex_all_val, file = file.path(my_script_is_using, paste(my_script_subbed, "_all_val_", sprintf("%.06f", best_mcc3_all_val), "_", best_count3_all_val, ".csv", sep = "")), row.names = FALSE)
+    
+    # Create submissions using full trained model using total validated positive count
+    best_mcc_extra <- mini_preds[sum(temp_positives) + 1]
+    submission0_extra <- fread("datasets/sample_submission.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+    submission0_extra$Response <- as.numeric(predictedValues > best_mcc_extra)
+    best_count_extra <- sum(submission0_extra$Response == 1)
+    cat("Submission with all data by taking the sum of positives of validated positives: ", best_count_extra, ". Threshold=", best_mcc_extra, "\n\n", sep = "")
+    write.csv(submission0_extra, file = file.path(my_script_is_using, paste(my_script_subbed, "_extra_", sprintf("%.06f", best_mcc_extra), "_", best_count_extra, ".csv", sep = "")), row.names = FALSE)
+    
+  }
+  
+  # End of Markdown
+  cat("```")
+  
+  return("Done")
+  
+}
