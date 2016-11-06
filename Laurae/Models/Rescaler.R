70d8a3e3df340bb5b10b4d11aef73937d5ca94cc
Laurae <Laurae2@users.noreply.github.com>
2016-11-07 00:49:38 +0100

    Update Rescaler.R

--- Laurae/Models/Rescaler.R
+++ Laurae/Models/Rescaler.R
@@ -1,4 +1,5 @@
 # Rescales predictions
+# Folds must be a list containing in each vector of the list the indexes of the folds
 # Please input CV_Preds = CV predictions in a list like LightGBM output: [[1]]=fold1 preds, [[2]]=fold2 preds...
 # CV Vector sizes: 236748, 237649, 236750, 236750, 236750
 # Please input Test_Preds = Per-fold predictions in a list like LightGBM output: [[1]]=fold1 preds, [[2]]=fold2 preds...
@@ -14,13 +15,13 @@ Rescaler <- function(folds, CV_Preds, Test_Preds) {
   All_Scale <- list()
   CV_All <- numeric(1183747)
   Test_Scale <- numeric(1183748)
-  for (i in 1:5) {
+  for (i in 1:length(folds)) {
     All_Scale[[i]] <- c(CV_Preds[[i]], Test_Preds[[i]])
     All_Scale[[i]] <- rank(All_Scale[[i]], ties.method = "average") / length(All_Scale[[i]])
     CV_Preds[[i]] <- All_Scale[[i]][1:length(CV_Preds[[i]])]
     CV_All[folds[[i]]] <- CV_Preds[[i]]
     Test_Preds[[i]] <- All_Scale[[i]][(length(CV_Preds[[i]]) + 1):length(All_Scale[[i]])]
-    Test_Scale <- Test_Scale + (Test_Preds[[i]] * 0.2)
+    Test_Scale <- Test_Scale + (Test_Preds[[i]] / length(folds))
   }
   returned <- list()
   returned[["CV"]] <- CV_All
